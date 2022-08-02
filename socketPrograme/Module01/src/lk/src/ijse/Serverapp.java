de06f893b1ac06af53c6c1f9fdebd15a03551f2b
NimmiShashikala60 <nimmishashikala385@gmail.com>
2022-08-02 22:29:46 +0530

    new update

--- socketPrograme/Module01/src/lk/src/ijse/Serverapp.java
+++ socketPrograme/Module01/src/lk/src/ijse/Serverapp.java
@@ -1,9 +1,6 @@
 package lk.src.ijse;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,13 +12,29 @@ public class Serverapp {
         System.out.println("Server is ranning in port : " + PORT);
 
         Socket localSocket = serverSocket.accept();
-        System.out.println("Port"+localSocket.getPort());
-        System.out.println("Ip"+localSocket.getInetAddress());
+       // System.out.println("Port"+localSocket.getPort());
+       // System.out.println("Ip"+localSocket.getInetAddress());
+        System.out.println("client accept..");
 
-        InputStreamReader inputStreamReader = new InputStreamReader(localSocket.getInputStream());
+       // InputStreamReader inputStreamReader = new InputStreamReader(localSocket.getInputStream());
+       // BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+       // String clientMag = bufferedReader.readLine();
+
+        //System.out.println("client says: "+clientMag);
+        DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
+        DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());
+
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String clientMag = bufferedReader.readLine();
 
-        System.out.println("client says: "+clientMag);
+        String massage = "",reply="";
+
+        while (!massage.equals("finish")){
+              massage=dataInputStream.readUTF();
+            System.out.println(massage);
+            reply=bufferedReader.readLine();
+            dataOutputStream.writeUTF(reply);
+            dataOutputStream.flush();
+        }
     }
 }

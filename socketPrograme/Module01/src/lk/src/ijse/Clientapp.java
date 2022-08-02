613a3518057e833d299f3d5ffd09a4cfa27ed87b
NimmiShashikala60 <nimmishashikala385@gmail.com>
2022-08-02 22:54:44 +0530

    new update

--- socketPrograme/Module01/src/lk/src/ijse/Clientapp.java
+++ socketPrograme/Module01/src/lk/src/ijse/Clientapp.java
@@ -1,16 +1,39 @@
 package lk.src.ijse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class Clientapp {
-    public static void main(String [] args) throws IOException {
+    public static void main(String[] args) {
         final int PORT = 8000;
-        Socket socket = new Socket("localhost",PORT);
+        try {
+            Socket   socket = new Socket("localhost", PORT);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
 
-        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-        printWriter.println("Hello server,kohomada oyta");
-        printWriter.flush();
+            //InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String massage = "", reply = "";
+
+            while (!massage.equals("finish")) {
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                massage = dataInputStream.readUTF();
+                System.out.println(massage);
+                dataOutputStream.flush();
+            }
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+
+            //PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+           // printWriter.println("Hello server,kohomada oyta");
+           // printWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
